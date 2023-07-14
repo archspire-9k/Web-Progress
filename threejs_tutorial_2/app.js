aa30747fe1aff4c08fdffec05b56a7a23fd89b02
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-07-14 06:39:06 -0400

    added files back

--- threejs_tutorial_2/app.js
+++ threejs_tutorial_2/app.js
@@ -1,21 +1,53 @@
 import * as THREE from 'three';
 import { gsap } from "gsap";
 import { Expo } from 'gsap';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-);
-camera.position.z = 5;
+
 
 var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setClearColor("#e5e5e5");
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
+var scene = new THREE.Scene();
+
+// Optional: Provide a DRACOLoader instance to decode compressed mesh data
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('/examples/jsm/libs/draco/');
+
+const loader = new GLTFLoader();
+loader.setDRACOLoader(dracoLoader);
+loader.setPath('./isometric_office/');
+loader.load('untitled.gltf', function (gltf) {
+
+    scene.add(gltf.scene);
+
+    render();
+
+});
+
+
+var camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000
+);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+camera.position.set(-5, 5, 19);
+controls.update();
+
+let cameraRadius = 20;
+let cameraAzimuth = 0;
+let cameraElevation = 0;
+let isMouseDown = false;
+let previousMouseX = 0;
+let previousMouseY = 0;
 
 // trigger renderer size camera aspect upon browser resizing
 window.addEventListener('resize', () => {
@@ -37,54 +69,42 @@ function onPointerMove(event) {
     pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
     pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
 
-    // update the picking ray with the camera and pointer position
-    raycaster.setFromCamera(pointer, camera);
+    // // update the picking ray with the camera and pointer position
+    // raycaster.setFromCamera(pointer, camera);
 
-    // calculate objects intersecting the picking ray
-    var intersects = raycaster.intersectObjects(scene.children, true);
+    // // calculate objects intersecting the picking ray
+    // var intersects = raycaster.intersectObjects(scene.children, true);
 
-    for (let i = 0; i < intersects.length; i++) {
-        // change color on method call
-        // intersects[i].object.material.color.set(0xff0000);
+    // for (let i = 0; i < intersects.length; i++) {
+    //     // change color on method call
+    //     // intersects[i].object.material.color.set(0xff0000);
 
-        var tl = gsap.timeline();
-        tl.to(intersects[i].object.scale, { duration: 1, x: 4, ease: Expo.easeOut });
-        tl.to(intersects[i].object.scale, { duration: 0.5, x: 0.5, ease: Expo.easeOut });
-        tl.to(intersects[i].object.position, { duration: 2, x: 2, ease: Expo.easeOut });
-        tl.to(intersects[i].object.rotation, { duration: 0.25, y: Math.PI * .5, ease: Expo.easeOut }, "=-1.5");
+    //     var tl = gsap.timeline();
+    //     tl.to(intersects[i].object.scale, { duration: 1, x: 4, ease: Expo.easeOut });
+    //     tl.to(intersects[i].object.scale, { duration: 0.5, x: 0.5, ease: Expo.easeOut });
+    //     tl.to(intersects[i].object.position, { duration: 2, x: 2, ease: Expo.easeOut });
+    //     tl.to(intersects[i].object.rotation, { duration: 0.25, y: Math.PI * .5, ease: Expo.easeOut }, "=-1.5");
+    // }
+    if (isMouseDown) {
+
     }
 }
 
-// define shape, material, and create mesh from them
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshLambertMaterial({ color: 0xf7f7f7 });
-
-var boxX = -10;
-for (let i = 0; i < 15; i++) {
-    var box = new THREE.Mesh(geometry, material);
-    box.position.x = (Math.random() - 0.5) * 10;
-    box.position.y = (Math.random() - 0.5) * 10;
-    box.position.z = (Math.random() - 0.5) * 10;
-    scene.add(box);
-    boxX += 1;
+function onPointerDown() {
+    isMouseDown = true;
 }
 
-// position
-// box.position.set(-2, 2, 0);
-
-// rotation
-// box.rotation.set(45, 0, 0);
-
-// scale
-// box.scale.set(1, 2, 1);
+function onPointerUp() {
+    isMouseUp = false;
+}
 
 var light = new THREE.PointLight(0xFFFFFF, 1, 500);
-light.position.set(0, 0, 0);
+light.position.set(0, -5, 0);
 
 scene.add(light);
 
 var light = new THREE.PointLight(0xFFFFFF, 2, 1000);
-light.position.set(0, 0, 25);
+light.position.set(0, 5, 25);
 
 scene.add(light);
 
