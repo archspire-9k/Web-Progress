2234e24f6d4f99692482811b269c9e8660fc0360
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-07-16 04:50:37 -0400

    added third tutorial

---
+++ threejs_tutorial_3/app.js
@@ -0,0 +1,120 @@
+import * as THREE from 'three';
+import { gsap } from "gsap";
+import { Expo } from 'gsap';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+
+
+
+var renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setClearColor("#e5e5e5");
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+var scene = new THREE.Scene();
+
+const axis = new THREE.AxesHelper(3);
+scene.add(axis);
+
+const gridHelper = new THREE.GridHelper(30);
+scene.add(gridHelper);
+
+const planeGeometry = new THREE.PlaneGeometry(30, 30);
+const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, side: THREE.DoubleSide })
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(plane);
+plane.rotation.x = -0.5 * Math.PI;
+
+const boxGeometry = new THREE.BoxGeometry(3, 3, 3);
+const boxMaterial = new THREE.MeshBasicMaterial({ color: 0xFFFFF })
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+
+scene.add(box);
+
+const sphereGeometry = new THREE.SphereGeometry(6, 3, 12);
+const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xFFF, wireframe: true });
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+
+scene.add(sphere);
+
+var camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000
+);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+camera.position.set(0, 2, 40);
+controls.update();
+
+let isMouseDown = false;
+
+// trigger renderer size camera aspect upon browser resizing
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+
+    camera.updateProjectionMatrix();
+})
+
+// implement raycaster to allow cursor-object interaction 
+var raycaster = new THREE.Raycaster();
+var pointer = new THREE.Vector2();
+
+function onPointerMove(event) {
+
+    // calculate pointer position in normalized device coordinates
+    // (-1 to +1) for both components
+    event.preventDefault();
+    pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+    pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
+
+    // // update the picking ray with the camera and pointer position
+    // raycaster.setFromCamera(pointer, camera);
+
+    // // calculate objects intersecting the picking ray
+    // var intersects = raycaster.intersectObjects(scene.children, true);
+
+    // for (let i = 0; i < intersects.length; i++) {
+    //     // change color on method call
+    //     // intersects[i].object.material.color.set(0xff0000);
+
+    if (isMouseDown) {
+
+    }
+}
+
+function onPointerDown() {
+    isMouseDown = true;
+}
+
+function onPointerUp() {
+    isMouseUp = false;
+}
+
+var light = new THREE.PointLight(0xFFFFFF, 1, 500);
+light.position.set(0, -5, 0);
+
+scene.add(light);
+
+var light = new THREE.PointLight(0xFFFFFF, 2, 1000);
+light.position.set(0, 5, 25);
+
+scene.add(light);
+
+var render = function () {
+    requestAnimationFrame(render);
+
+    // put animation(s) here
+    box.rotation.x += 0.05;
+    box.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+}
+
+render();
+
+// window.addEventListener('mousemove', onPointerMove);
