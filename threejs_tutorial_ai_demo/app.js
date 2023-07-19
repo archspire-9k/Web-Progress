f8b4f40d38f7ab159ac83e6c1a3455f73aa3fb63
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-07-19 06:36:36 -0400

    add path

--- threejs_tutorial_ai_demo/app.js
+++ threejs_tutorial_ai_demo/app.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import * as dat from 'dat.gui';
+import * as YUKA from 'yuka';
 
 var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setClearColor("#e5e5e5");
@@ -17,6 +19,40 @@ var camera = new THREE.PerspectiveCamera(
 camera.position.set(0, 20, 0);
 camera.lookAt(scene.position);
 
+const vehicleGeometry = new THREE.ConeGeometry(0.1, 0.5, 8);
+vehicleGeometry.rotateX(Math.PI/2);
+const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+const vehicleMesh = new THREE.Mesh(vehicleGeometry, material); 
+vehicleMesh.matrixAutoUpdate = false;
+scene.add(vehicleMesh);
+
+const vehicle = new YUKA.Vehicle();
+vehicle.setRenderComponent(vehicleMesh, sync);
+
+function sync(entity, renderComponent) {
+    renderComponent.matrix.copy(entity.worldMatrix);
+}
+
+const path = new YUKA.Path();
+path.add(new YUKA.Vector3(-4, 0, 4));
+path.add(new YUKA.Vector3(-6, 0, 0));
+path.add(new YUKA.Vector3(-4, 0, -4));
+path.add(new YUKA.Vector3(0, 0, 0));
+path.add(new YUKA.Vector3(4, 0, -4));
+path.add(new YUKA.Vector3(6, 0, 0));
+path.add(new YUKA.Vector3(4, 0, 4));
+path.add(new YUKA.Vector3(0, 0, 6));
+
+vehicle.position.copy(path.current());
+
+const followPathBehavior = new YUKA.FollowPathBehavior(path, 0.5);
+vehicle.steering.add(followPathBehavior);
+
+const entityManager = new YUKA.EntityManager();
+entityManager.add(vehicle);
+
+const time = new YUKA.Time();
+
 // trigger renderer size camera aspect upon browser resizing
 window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -33,7 +69,10 @@ scene.add(light);
 var render = function () {
     requestAnimationFrame(render);
 
+    const delta = time.update().getDelta();
+    entityManager.update(delta);
     renderer.render(scene, camera);
 }
 
 render();
+// renderer.setAnimationLoop(render);
