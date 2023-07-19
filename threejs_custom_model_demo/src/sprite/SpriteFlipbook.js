1f82c2ee77402062da6ad7aed79e2bc66ef959bc
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-07-19 05:03:51 -0400

    debug sprite not loading

--- threejs_custom_model_demo/src/sprite/SpriteFlipbook.js
+++ threejs_custom_model_demo/src/sprite/SpriteFlipbook.js
@@ -9,13 +9,14 @@ export class SpriteFlipbook {
     sprite;
 
     constructor(spriteTexture, tileX, tileY, scene) {
+        this.tileX = tileX;
+        this.tileY = tileY;
         this.map = new THREE.TextureLoader().load(spriteTexture);
         this.map.magFilter = THREE.NearestFilter;
         this.map.repeat.set(1 / tileX, 1 / tileY);
-        
         this.update(0);
 
-        const material = new THREE.SpriteMaterial({ map: map });
+        const material = new THREE.SpriteMaterial({ map: this.map, color: 0xffffff });
 
         this.sprite = new THREE.Sprite(material);
         this.sprite.position.y = 1.2;
