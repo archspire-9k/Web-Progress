7c596ea983c25e4e7be093523204d3ae73e31040
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-07-19 05:44:28 -0400

    remove gsap and add yuka

--- threejs_tutorial_ai_demo/app.js
+++ threejs_tutorial_ai_demo/app.js
@@ -1,7 +1,5 @@
 import * as THREE from 'three';
 
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-
 var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setClearColor("#e5e5e5");
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -10,16 +8,14 @@ document.body.appendChild(renderer.domElement);
 var scene = new THREE.Scene();
 
 var camera = new THREE.PerspectiveCamera(
-    75,
+    45,
     window.innerWidth / window.innerHeight,
     1,
     1000
 );
 
-const controls = new OrbitControls(camera, renderer.domElement);
-
-camera.position.set(-5, 5, 19);
-controls.update();
+camera.position.set(0, 20, 0);
+camera.lookAt(scene.position);
 
 // trigger renderer size camera aspect upon browser resizing
 window.addEventListener('resize', () => {
@@ -28,22 +24,6 @@ window.addEventListener('resize', () => {
 
     camera.updateProjectionMatrix();
 })
-
-// implement raycaster to allow cursor-object interaction 
-var pointer = new THREE.Vector2();
-
-function onPointerMove(event) {
-
-    // calculate pointer position in normalized device coordinates
-    // (-1 to +1) for both components
-    event.preventDefault();
-    pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
-    pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
-
-    if (isMouseDown) {
-
-    }
-}
 
 var light = new THREE.PointLight(0xFFFFFF, 1, 500);
 light.position.set(0, 25, 0);
@@ -57,5 +37,3 @@ var render = function () {
 }
 
 render();
-
-// window.addEventListener('mousemove', onPointerMove);
