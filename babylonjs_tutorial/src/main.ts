6f1dec855f135baee299357ff4a38ff5643d843b
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-07-20 05:01:49 -0400

    add new heightmap

--- babylonjs_tutorial/src/main.ts
+++ babylonjs_tutorial/src/main.ts
@@ -7,24 +7,34 @@ const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
 const engine = new BABYLON.Engine(canvas);
 
 const scene = createScene(engine);
+scene.clearColor = new BABYLON.Color4(0, 0, 0, 255);
 
-scene.createDefaultLight();
+// const light = BABYLON.
+scene.createDefaultEnvironment();
 
-const camera = new BABYLON.UniversalCamera('camera', new BABYLON.Vector3(0, 3, -10), scene);
+const camera = new BABYLON.ArcRotateCamera('camera', 0, Math.PI/2, 8, new BABYLON.Vector3(0, 0.5, 0), scene);
 camera.attachControl(true);
 camera.inputs.addMouseWheel();
-camera.setTarget(BABYLON.Vector3.Zero());
 
 // sphere mesh
-const sphere = BABYLON.MeshBuilder.CreateSphere('sphere', {diameter: 1, segments: 32}, scene);
-sphere.position.set(0, 2, 2);
+const sphere = BABYLON.MeshBuilder.CreateSphere('sphere', { diameter: 1, segments: 64 }, scene);
+sphere.position.set(0, 0.5, 0);
+
+const sphereMaterial = new BABYLON.PBRMaterial('sphereMaterial', scene);
+sphereMaterial.roughness = 0;
+sphereMaterial.metallic = 0;
+sphereMaterial.subSurface.isRefractionEnabled = true;
+sphereMaterial.subSurface.indexOfRefraction = 1.5;
+sphereMaterial.subSurface.tintColor = new BABYLON.Color3(239, 239, 0);
+
+sphere.material = sphereMaterial;
+
 
 // Built-in 'ground' shape.
-const ground = BABYLON.MeshBuilder.CreateGroundFromHeightMap("ground", "./src/assets/img/heightmap.png",
-{width: 6, height: 6, subdivisions: 50}, scene);
+const ground = BABYLON.MeshBuilder.CreateGroundFromHeightMap("ground", "./src/assets/img/waves.png",
+    { width: 5, height: 5, subdivisions: 50 }, scene);
 
-ground.material = new BABYLON.StandardMaterial("groundMaterial" , scene);
-// ground.material.wireframe = true;
+ground.material = new BABYLON.StandardMaterial("groundMaterial", scene);
 
 engine.runRenderLoop(() => {
     scene.render();
