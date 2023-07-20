4539f5369d7d8821bfbf7200e1a362f569e372af
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-07-20 00:23:03 -0400

    add camera

---
+++ babylonjs_tutorial/src/createScene.ts
@@ -0,0 +1,14 @@
+import * as BABYLON from '@babylonjs/core';
+
+export default function createScene(engine: BABYLON.Engine) {
+    const scene = new BABYLON.Scene(engine);
+
+    //light 
+    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+    light.intensity = 0.7;
+
+    // sphere mesh
+    const sphere = new BABYLON.MeshBuilder.CreateSphere('sphere', {diameter: 2, segments: 32}, scene);
+
+    return scene;
+}
--- babylonjs_tutorial/src/main.ts
+++ babylonjs_tutorial/src/main.ts
@@ -1,24 +1,22 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import * as BABYLON from '@babylonjs/core';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import createScene from './createScene';
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
+
+const engine = new BABYLON.Engine(canvas);
+
+const scene = createScene(engine);
+
+// Creates and positions a free camera
+const camera = new BABYLON.FreeCamera("camera1",
+    new BABYLON.Vector3(0, 5, -10), scene);
+// Targets the camera to scene origin
+camera.setTarget(BABYLON.Vector3.Zero());
+// This attaches the camera to the canvas
+camera.attachControl(canvas, true);
+
+
+engine.runRenderLoop(() => {
+    scene.render();
+});
