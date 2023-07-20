346e5cee35e9918cc263a40f7479e02723eabf5d
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-07-20 06:37:34 -0400

    add gizmo to manipulate object

--- babylonjs_tutorial/src/createScene.ts
+++ babylonjs_tutorial/src/createScene.ts
@@ -3,5 +3,9 @@ import * as BABYLON from '@babylonjs/core';
 export default function createScene(engine: BABYLON.Engine) {
     const scene = new BABYLON.Scene(engine);
 
+    //light 
+    // const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 0, 0), scene);
+    // light.intensity = 2;
+
     return scene;
 }
--- babylonjs_tutorial/src/main.ts
+++ babylonjs_tutorial/src/main.ts
@@ -9,10 +9,9 @@ const engine = new BABYLON.Engine(canvas);
 const scene = createScene(engine);
 scene.clearColor = BABYLON.Color4.FromHexString('#868990');
 
-var lightRed = new BABYLON.PointLight("Light", new BABYLON.Vector3(0, 0.5, 0), scene);
+const lightRed = new BABYLON.PointLight("Light", new BABYLON.Vector3(0, 0.5, 0), scene);
 lightRed.diffuse = BABYLON.Color3.FromHexString('#eb4225');
 lightRed.specular = new BABYLON.Color3(0, 0, 0);
-
 
 const camera = new BABYLON.ArcRotateCamera('camera', 0, Math.PI / 2, 8, new BABYLON.Vector3(0, 0.5, 0), scene);
 camera.attachControl(true);
@@ -20,13 +19,17 @@ camera.inputs.addMouseWheel();
 
 // sphere mesh
 const sphere = BABYLON.MeshBuilder.CreateSphere('sphere', { diameter: 1, segments: 64 }, scene);
-sphere.position.set(0, 0.5, 0);
+sphere.position = lightRed.position;
 
 const sphereMaterial = new BABYLON.StandardMaterial('sphereMaterial', scene);
-sphereMaterial.diffuseColor = BABYLON.Color3.FromInts(238, 237, 171);
+sphereMaterial.emissiveColor = BABYLON.Color3.FromInts(238, 237, 171);
 
 sphere.material = sphereMaterial;
 
+const utilLayer = new BABYLON.UtilityLayerRenderer(scene);
+
+const positionGizmo = new BABYLON.PositionGizmo(utilLayer);
+positionGizmo.attachedMesh = sphere;
 
 // Built-in 'ground' shape.
 const ground = BABYLON.MeshBuilder.CreateGroundFromHeightMap("ground", "./src/assets/img/waves.png",
@@ -34,6 +37,8 @@ const ground = BABYLON.MeshBuilder.CreateGroundFromHeightMap("ground", "./src/as
 
 var groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
 groundMaterial.diffuseColor = BABYLON.Color3.FromInts(42, 50, 61);
+groundMaterial.specularColor = new BABYLON.Color3(1, 1, 1);
+
 ground.material = groundMaterial;
 
 engine.runRenderLoop(() => {
