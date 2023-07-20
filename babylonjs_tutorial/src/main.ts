1b7920876f17a433cd861b6f43b49f8bc90214ce
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-07-20 00:58:29 -0400

    create meshes

--- babylonjs_tutorial/src/main.ts
+++ babylonjs_tutorial/src/main.ts
@@ -8,15 +8,30 @@ const engine = new BABYLON.Engine(canvas);
 
 const scene = createScene(engine);
 
-// Creates and positions a free camera
-const camera = new BABYLON.FreeCamera("camera1",
-    new BABYLON.Vector3(0, 5, -10), scene);
-// Targets the camera to scene origin
-camera.setTarget(BABYLON.Vector3.Zero());
-// This attaches the camera to the canvas
-camera.attachControl(canvas, true);
+scene.createDefaultCameraOrLight(true, false, true);
+// // Creates and positions a free camera
+// const camera = new BABYLON.FreeCamera("camera1",
+//     new BABYLON.Vector3(0, 5, -10), scene);
+// // Targets the camera to scene origin
+// camera.setTarget(BABYLON.Vector3.Zero());
+// // This attaches the camera to the canvas
+// camera.attachControl(canvas, true);
 
+// sphere mesh
+const sphere = BABYLON.MeshBuilder.CreateSphere('sphere', {diameter: 1, segments: 32}, scene);
+
+// Built-in 'ground' shape.
+const ground = BABYLON.MeshBuilder.CreateGround("ground", 
+{width: 6, height: 6, subdivisions: 30}, scene);
+
+ground.material = new BABYLON.StandardMaterial("groundMaterial" , scene);
+ground.material.wireframe = true;
 
 engine.runRenderLoop(() => {
     scene.render();
+});
+
+// Watch for browser/canvas resize events
+window.addEventListener("resize", function () {
+    engine.resize();
 });
