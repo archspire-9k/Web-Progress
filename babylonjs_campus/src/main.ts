016f55d514d0a8feba33aa7e1e14e89590ec863f
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-07-21 03:51:36 -0400

    make the scene a class instead

--- babylonjs_campus/src/main.ts
+++ babylonjs_campus/src/main.ts
@@ -1,51 +1,80 @@
-import { Color3, Engine, HemisphericLight, LightGizmo, MeshBuilder, PointLight, StandardMaterial, UtilityLayerRenderer, Vector3 } from '@babylonjs/core';
+import { Color3, Engine, HemisphericLight, LightGizmo, MeshBuilder, PointLight, Scene, StandardMaterial, UtilityLayerRenderer, Vector3 } from '@babylonjs/core';
 import "@babylonjs/loaders/glTF";
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 
 import createScene from './createScene';
 
-const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
 
-const engine = new Engine(canvas);
 
-const scene = createScene(engine);
 
 // enum for states
 enum State { START = 0, GAME = 1, LOSE = 2, CUTSCENE = 3 };
 
-// render meshes here
-// TODO: change this to a function
-const box = MeshBuilder.CreateBox("box", { size: 0.25 }, scene)
-const boxMaterial = new StandardMaterial('boxMaterial', scene);
-boxMaterial.diffuseColor = Color3.Gray();
-box.material = boxMaterial;
+class App {
+    // General Entire Application
+    private _scene: Scene;
+    private _canvas: HTMLCanvasElement;
+    private _engine: Engine;
 
-// render light here
-// const targetLight = new PointLight("light", new Vector3(0, 0.5, 0), scene);
-const light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+    //Scene - related
+    private _state: number = 0;
+    private _gamescene: Scene;
+    private _cutScene: Scene;
 
-const utilLayer = new UtilityLayerRenderer(scene);
-const lightGizmo: LightGizmo = new LightGizmo(utilLayer);
-lightGizmo.light = light1;
+    //post process
+    private _transition: boolean = false;
 
-engine.runRenderLoop(() => {
-    scene.render();
-});
+    constructor() {
+        this._canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
+        this._engine = new Engine(this._canvas);
+        this._scene = createScene(this._engine);
 
-// Watch for browser/canvas resize events
-window.addEventListener("resize", function () {
-    engine.resize();
-});
+        // Watch for browser/canvas resize events
+        window.addEventListener("resize", () => {
+            this._engine.resize();
+        });
 
-// hide/show the Inspector
-window.addEventListener("keydown", (ev) => {
-    // Shift+Ctrl+Alt+I
-    if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.key === "I") {
-        if (scene.debugLayer.isVisible()) {
-            scene.debugLayer.hide();
-        } else {
-            scene.debugLayer.show();
-        }
+        // hide/show the Inspector
+        window.addEventListener("keydown", (ev) => {
+            // Shift+Ctrl+Alt+I
+            if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.key === "I") {
+                if (this._scene.debugLayer.isVisible()) {
+                    this._scene.debugLayer.hide();
+                } else {
+                    this._scene.debugLayer.show();
+                }
+            }
+        });
+
+        // implement main logic here
+        this._mainLogic();
+        console.log(this._scene);
     }
-});
+
+    private _mainLogic() {
+
+        // render meshes here
+        // TODO: change this to a function
+        const box = MeshBuilder.CreateBox("box", { size: 0.25 }, this._scene)
+        const boxMaterial = new StandardMaterial('boxMaterial', this._scene);
+        boxMaterial.diffuseColor = Color3.Gray();
+        box.material = boxMaterial;
+
+        // render light here
+        // const targetLight = new PointLight("light", new Vector3(0, 0.5, 0), scene);
+        const light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), this._scene);
+
+        const utilLayer = new UtilityLayerRenderer(this._scene);
+        const lightGizmo: LightGizmo = new LightGizmo(utilLayer);
+        lightGizmo.light = light1;
+
+        this._engine.runRenderLoop(() => {
+            this._scene.render();
+        });
+    }
+}
+
+new App();
+
+
--- babylonjs_campus/src/stateMachines/goToStart.ts
+++ babylonjs_campus/src/stateMachines/goToStart.ts
@@ -1,5 +1,13 @@
-import { Engine } from "@babylonjs/core";
+import { Color4, Engine, FreeCamera, Scene, Vector3 } from "@babylonjs/core";
 
-export default function goToStart(engine: Engine) {
+export default function goToStart(engine: Engine, scene: Scene) {
     engine.displayLoadingUI();
+
+    //--SCENE SETUP--
+    //dont detect any inputs from this ui while the game is loading
+    scene.detachControl();
+    let newScene = new Scene(engine);
+    scene.clearColor = new Color4(0, 0, 0, 1);
+    let camera = new FreeCamera("camera1", new Vector3(0, 0, 0), newScene);
+    camera.setTarget(Vector3.Zero());
 }
