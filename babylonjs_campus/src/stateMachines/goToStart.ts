966723504669d6aed38ee4702dae48d418ed497f
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-07-21 05:14:42 -0400

    add gui play button

--- babylonjs_campus/src/stateMachines/goToStart.ts
+++ babylonjs_campus/src/stateMachines/goToStart.ts
@@ -1,8 +1,9 @@
 import { Engine, Scene } from "@babylonjs/core";
 import { AdvancedDynamicTexture } from "@babylonjs/gui/2D/advancedDynamicTexture";
-
 import createScene from "../createScene";
 import State from "./State";
+import { Control } from "@babylonjs/gui/2D/controls/control";
+import { Button } from "@babylonjs/gui/2D/controls/button";
 
 export default async function _goToStart(engine: Engine, scene: Scene) {
     engine.displayLoadingUI();
@@ -15,6 +16,16 @@ export default async function _goToStart(engine: Engine, scene: Scene) {
     //--GUI--
     const guiMenu = AdvancedDynamicTexture.CreateFullscreenUI("UI");
     guiMenu.idealHeight = 720;
+
+    //create a simple button
+    const startBtn = Button.CreateSimpleButton("start", "PLAY");
+    startBtn.width = 0.2;
+    startBtn.height = "40px";
+    startBtn.color = "white";
+    startBtn.top = "-14px";
+    startBtn.thickness = 0;
+    startBtn.verticalAlignment = Control.VERTICAL_ALIGNMENT_BOTTOM;
+    guiMenu.addControl(startBtn);
 
     await newScene.whenReadyAsync();
     engine.hideLoadingUI();
