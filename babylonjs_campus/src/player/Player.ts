e13e8fd803c822d5e7a3fdc30e9920a833f84531
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-07-26 00:30:39 -0400

    Update Player.ts

--- babylonjs_campus/src/player/Player.ts
+++ babylonjs_campus/src/player/Player.ts
@@ -1,4 +1,4 @@
-import { Scene, Sprite, TransformNode } from "@babylonjs/core";
+import { Scene, Sprite, TransformNode, Vector2 } from "@babylonjs/core";
 import CharacterMovement from "./CharacterMovement";
 
 export default class Player {
@@ -13,15 +13,44 @@ export default class Player {
     //animations
 
     //TODO: add walk animation
-    // private _walk() {
-    // }
+    private _walk() {
+
+    }
 
     private _idle() {
-        this._player.playAnimation(0, 3, true, 375);
+        this._player.playAnimation(0, 0, true, 375);
     };
+
+    //const values
+    private static readonly PLAYER_SPEED: number = 0.45;
+
+    //player movement vars
+    private _deltaTime: number = 0;
+    private _h: number;
+    private _v: number;
+
+    private _moveDirection: Vector2 = new Vector2();
+    private _inputAmt: number;
 
     constructor(scene: Scene, player: Sprite) {
         this._input = new CharacterMovement(scene);
         this._player = player;
+
+
+    }
+
+    private _updatePosition() {
+        this._moveDirection = Vector2.Zero(); // vector that holds movement information
+        this._h = this._input.inputVector.x; //x-axis
+        this._v = this._input.inputVector.y; //z-axis
+
+        //--MOVEMENTS BASED ON CAMERA (as it rotates)--
+        let fwd = this._camRoot.forward;
+        let right = this._camRoot.right;
+        let correctedVertical = fwd.scaleInPlace(this._v);
+        let correctedHorizontal = right.scaleInPlace(this._h);
+
+        //movement based off of camera's view
+        let move = correctedHorizontal.addInPlace(correctedVertical);
     }
 }
